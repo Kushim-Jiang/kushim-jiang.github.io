440b788e18f6d105347d2a9092648415f1ccff4a
Kushim-Jiang <kushim_jiang@outlook.com>
2026-05-08 10:11:35 +0800

    Update roadmap

--- data/roadmap.py
+++ data/roadmap.py
@@ -23,7 +23,7 @@ def _parse_roadmap_page(url: str) -> list:
     result = []
     entries = tree.cssselect("#container > div.plane.shadow > svg > g > g")
     if not entries:
-        print(f"⚠️ cannot find target element：{url}")
+        print(f"⚠️ cannot find target element: {url}")
         return result
     for entry in entries:
         if entry is None:
@@ -75,14 +75,6 @@ def _parse_roadmap_page(url: str) -> list:
         if a_elem is not None:
             url = a_elem.get("href")
 
-        # HACK
-        if url == "170-A44":
-            print(f"⚠️ HACK: wrong url for Egyptian Hieroglyphs Format Controls: {url}")
-            url = "https://www.unicode.org/charts/PDF/U13430.pdf"
-        elif url == "167-A37":
-            print(f"⚠️ HACK: wrong url for Kanbun: {url}")
-            url = "https://www.unicode.org/charts/PDF/U3190.pdf"
-
         item = {
             "name": name,
             "short": short,
@@ -114,7 +106,15 @@ def parse_roadmap() -> None:
         all_data.extend(_parse_roadmap_page(link))
     print(f"\n✅ parsed {len(all_data)} blocks of encoding data")
     with open("assets/json/roadmap.json", "w", encoding="utf-8") as f:
-        json.dump({"date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "data": all_data}, f, ensure_ascii=False, indent=2)
+        json.dump(
+            {
+                "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                "data": all_data
+            },
+            f,
+            ensure_ascii=False,
+            indent=2,
+        )
 
 
 class RoadmapBlock(TypedDict):
@@ -151,7 +151,7 @@ def get_names(roadmap: Roadmap, dump: bool = False) -> list[str]:
 def get_names_from_file(dir: str, dump: bool = False) -> None:
     roadmap = _load_roadmap(dir)
     names = get_names(roadmap, dump)
-    
+
     existing_data = {}
     try:
         with open("assets/json/roadmap_zh.json", "r", encoding="utf-8") as f:
@@ -159,14 +159,14 @@ def get_names_from_file(dir: str, dump: bool = False) -> None:
                 existing_data[item["name"]] = item
     except FileNotFoundError:
         pass
-    
+
     dicts = []
     for name in names:
         if name in existing_data:
             dicts.append(existing_data[name])
         else:
             dicts.append({"name": name, "zh-cn": "", "note": ""})
-    
+
     dicts.sort(key=lambda x: x["name"])
     with open("assets/json/roadmap_zh.json", "w", encoding="utf-8") as f:
         json.dump(dicts, f, ensure_ascii=False, indent=2)
@@ -180,7 +180,9 @@ def check_missing_names() -> None:
         roadmap_zh = json.load(f)
     roadmap_zh_names = [item["name"] for item in roadmap_zh]
 
-    missing_names = [name for name in roadmap_names if name not in roadmap_zh_names]
+    missing_names = [
+        name for name in roadmap_names if name not in roadmap_zh_names
+    ]
 
     if missing_names:
         print("⚠️ name not in roadmap_zh.json:")
